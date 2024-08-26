b0749189de1d5f1a0c67ba3a1f608017026d3410
sangamhoni <87515320+subsangam@users.noreply.github.com>
2024-08-26 14:24:40 -0500

    Complete Number Guessing Game

--- 01-number-guessing-game/main.py
+++ 01-number-guessing-game/main.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 # my functions
@@ -8,7 +9,6 @@ def is_int(value):
     except ValueError:
         return False
 
-
 # main program
 lowerbound=input("Enter the lower bound: ")
 upperbound=input("Enter the upper bound: ")
@@ -17,14 +17,40 @@ if (not is_int(upperbound) or not is_int(lowerbound)):
     print("Enter numbers as input")
     exit()
 
-elif (upperbound<lowerbound):
-    print("Upperbound is less than lowerbound")
+upperbound=int(upperbound)
+lowerbound=int(lowerbound)  
+
+if (upperbound<lowerbound):
+    print("Invalid range. Upperbound is less than lowerbound.")
+    exit()
+elif (upperbound-lowerbound<3):
+    print("Range too small to guess.")
     exit()
 
-print("worked")
+total_guess=math.log2((upperbound-1)-(lowerbound+1)+1)//1
+answer=random.randint(lowerbound+1, upperbound-1)
 
+print(f"\nYou have {int(total_guess)} tries to guess a number between {lowerbound} and {upperbound}.")
 
+guess_count=0
 
-
-
-
+while (guess_count!=total_guess):
+    guess=input("Guess a number: ")
+    # error handling for invalid input
+    if (not is_int(guess)):
+        print("Input should be an integer.")
+        exit()
+    
+    guess=int(guess)
+    guess_count=guess_count+1
+    
+    if (guess != answer):
+        if (guess > answer):
+            print("You guessed too high.")
+        else:
+            print("You guessed too small")
+    else:
+        print(f"Congratulations! You guessed it in {guess_count} tries.")
+        exit()
+        
+print(f"\nThe number was {answer}. Better Luck Next Time!")
